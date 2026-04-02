e73cd860abea1fafbbd9c5130056ed8dbc406431
Nicolas Fortin <nicolas.fortin37@gmail.com>
2026-04-02 18:41:34 +0200

    boostrap

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace SailwindRegatta
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "com.sailwindregatta.mod";
+        public const string PLUGIN_NAME = "Sailwind Regatta";
+        public const string PLUGIN_VERSION = "0.1.0";
+
+        internal static Plugin Instance { get; private set; }
+        internal static ManualLogSource Log { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            Log = Logger;
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PLUGIN_GUID);
+
+            Log.LogInfo($"{PLUGIN_NAME} v{PLUGIN_VERSION} loaded.");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SailwindRegatta")]
+[assembly: AssemblyDescription("Regatta mod for Sailwind")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SailwindRegatta")]
+[assembly: AssemblyCopyright("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

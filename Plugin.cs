069dd26d50af9be001c593edb277febb0de39365
Nicolas Fortin <nicolas.fortin37@gmail.com>
2026-04-19 20:56:53 +0200

    Release 0.2.0

--- Plugin.cs
+++ Plugin.cs
@@ -11,9 +11,9 @@ namespace SailwindRegatta
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        public const string PLUGIN_GUID = "com.sailwindregatta.mod";
+        public const string PLUGIN_GUID = "com.zallek.sailwindregatta";
         public const string PLUGIN_NAME = "Sailwind Regatta";
-        public const string PLUGIN_VERSION = "0.1.0";
+        public const string PLUGIN_VERSION = "0.2.0";
 
         internal static Plugin Instance { get; private set; }
         internal static ManualLogSource Log { get; private set; }
